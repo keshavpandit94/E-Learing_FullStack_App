e4cc4a4b24f3e99073b219a9c3c2043eaf137677
Keshav Chandra Pandit <134667288+keshavpandit94@users.noreply.github.com>
2026-05-11 00:22:36 +0530

    Update BACK_URL for the API endpoint

--- AdminFrontend/src/api.js
+++ AdminFrontend/src/api.js
@@ -1,4 +1,4 @@
-const BACK_URL = "https://elearningwithadmin.onrender.com";
+const BACK_URL = "https://e-learing-fullstack-app-backend.onrender.com";
 // const BACK_URL = "http://localhost:5000";
 
 export const ADMIN_TOKEN = "iamadmin94"
